666132fb77366af8eee91bae980b4f3b070916a5
Zac Mrowicki <zmrowicki@hotmail.com>
2015-03-15 13:12:44 -0700

    Add title functionality for show page. p98

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -11,5 +11,12 @@ RSpec.feature "Creating Projects" do
     click_button "Create Project"
 
     expect(page).to have_content("Project has been created.")
+
+    project = Project.find_by(name: "Sublime Text 3")
+
+    expect(page.current_url).to eql(project_url(project))
+
+    title = "Sublime Text 3 - Projects - Ticketee"
+    expect(page).to have_title(title)
   end
 end

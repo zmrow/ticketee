2db3c69d196c494143336af7de4e546a81b8bb80
Zac Mrowicki <zmrowicki@hotmail.com>
2015-03-15 13:41:48 -0700

    Add validation to ensure names are not blank.  Add error messages into view. p106

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
 RSpec.feature "Creating Projects" do
-  scenario "A user can create a new project" do
+  before do
     visit "/"
 
     click_link "New Project"
+  end
 
+  scenario "A user can create a new project" do
     fill_in "Name", with: "Sublime Text 3"
     fill_in "Description", with: "A text editor for everyone"
     click_button "Create Project"
@@ -18,5 +20,12 @@ RSpec.feature "Creating Projects" do
 
     title = "Sublime Text 3 - Projects - Ticketee"
     expect(page).to have_title(title)
+  end
+
+  scenario "A user cannot create a project without a name" do
+    click_button "Create Project"
+
+    expect(page).to have_content("Project has not been created.")
+    expect(page).to have_content("Name can't be blank")
   end
 end

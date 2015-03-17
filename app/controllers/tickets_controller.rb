ffcf05042ece28a5307efba2e0f0212118408ff4
Zac Mrowicki <zmrowicki@hotmail.com>
2015-03-16 21:58:36 -0700

    Add ticket model and controller.  Implement tests and build new and create actions in the controller. p165

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,29 @@
+class TicketsController < ApplicationController
+  before_action :set_project
+
+  def new
+    @ticket = @project.tickets.build
+  end
+
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+
+    if @ticket.save
+      flash[:notice] = "Ticket has been created."
+      redirect_to [@project, @ticket]
+    else
+      flash.now[:alert] = "Ticket has not been created."
+      render "new"
+    end
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :description)
+  end
+
+  def set_project
+   @project = Project.find(params[:project_id])
+  end 
+end
---
+++ spec/features/creating_tickets_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.feature "Creating Tickets" do
+  before do
+    FactoryGirl.create(:project, name: "Internet Explorer")
+
+    visit "/"
+    click_link "Internet Explorer"
+    click_link "New Ticket"
+  end
+
+  scenario "with valid attributes" do
+    fill_in "Title", with: "Non-standards compliance"
+    fill_in "Description", with: "My pages are ugly!"
+    click_button "Create Ticket"
+
+    expect(page).to have_content("Ticket has been created.")
+  end
+
+  scenario "with missing fields" do
+    click_button "Create Ticket"
+
+    expect(page).to have_content("Ticket has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Description can't be blank") 
+  end
+end

82f4fed93a0847688ae72b07d877d907f1c25bbc
Zac Mrowicki <zmrowicki@hotmail.com>
2015-03-19 19:20:49 -0700

    Fully implement creating tickets under a project

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
   before_action :set_project
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
   def new
     @ticket = @project.tickets.build
@@ -26,4 +27,8 @@ class TicketsController < ApplicationController
   def set_project
    @project = Project.find(params[:project_id])
   end 
+
+  def set_ticket
+    @ticket = @project.tickets.find(params[:id])
+  end
 end
--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -24,4 +24,13 @@ RSpec.feature "Creating Tickets" do
     expect(page).to have_content("Title can't be blank")
     expect(page).to have_content("Description can't be blank") 
   end
+
+  scenario "with an invalid description" do
+    fill_in "Title", with: "Non-standards compliance"
+    fill_in "Description", with: "It sucks"
+    click_button "Create Ticket"
+
+    expect(page).to have_content("Ticket has not been created")
+    expect(page).to have_content("Description is too short")
+  end
 end

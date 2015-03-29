a3a2c438b8baafe4881742be35c84d4ca96a61f3
Zac Mrowicki <zmrowicki@hotmail.com>
2015-03-29 10:39:47 -0700

    Add ability to create admin users in admin namespace. p240

--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Ad admin can create a new user" do
+RSpec.feature "An admin can create a new user" do
   let!(:admin) { FactoryGirl.create(:user, :admin) }
 
   before do
@@ -16,5 +16,15 @@ RSpec.feature "Ad admin can create a new user" do
     fill_in "Password", with: "password"
     click_button "Create User"
     expect(page).to have_content("User has been created.")
+  end
+
+  scenario "when the new user is an admin" do
+    fill_in "Email", with: "admin@example.com"
+    fill_in "Password", with: "password"
+    check "Is an admin?"
+    click_button "Create User"
+
+    expect(page).to have_content("User has been created.")
+    expect(page).to have_content("admin@example.com (Admin)")
   end
 end
